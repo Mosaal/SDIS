935553ca2942676724f2d4aa6ea74afa44469b97
Alexandre Saraiva Moreira <alexandremoreira_13@sapo.pt>
2017-02-13 20:15:41 +0000

    Client and Server classes

---
+++ L01/src/L01/Client.java
@@ -0,0 +1,9 @@
+package L01;
+
+public class Client {
+
+	public static void main(String[] args) {
+		new Client();
+	}
+	
+}
---
+++ L01/src/L01/Server.java
@@ -0,0 +1,9 @@
+package L01;
+
+public class Server {
+
+	public static void main(String[] args) {
+		new Server();
+	}
+	
+}
