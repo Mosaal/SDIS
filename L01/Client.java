888776b1759403831692c13c20bd9d43bafb502d
Alexandre Saraiva Moreira <alexandremoreira_13@sapo.pt>
2017-02-15 18:57:30 +0000

    Almost done

--- L01/Client.java
+++ L01/Client.java
@@ -1,8 +1,13 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Client {
 	
 	// Args
+	private static int portNumber;
 	private static String hostname;
-	private static String portNumber;
 	private static String plateNumber;
 	private static String ownerName;
 	
@@ -12,28 +17,7 @@ public class Client {
 	private static final byte OPER = 2;
 	private static final byte PLATE = 3;
 	private static final byte OWNER = 4;
-	
-	public static boolean procArgs(String[] args) {
-		if (args.length != 4 && args.length != 5)
-			return false;
-		
-		if (args[OPER].equals("register") && args.length == 5) {
-			hostname = args[HOST];
-			portNumber = args[PORT];
-			plateNumber = args[PLATE];
-			ownerName = args[OWNER];
-
-			return true;
-		} else if (args[OPER].equals("lookup") && args.length == 4) {
-			hostname = args[HOST];
-			portNumber = args[PORT];
-			plateNumber = args[PLATE];
-			
-			return true;
-		}
-		
-		return false;
-	}
+	private static final int MAX_SIZE = 256;
 	
 	public static void printUsage() {
 		System.out.println("Usage: java Client <host_name> <port_number> <oper> <opnd>");
@@ -45,15 +29,58 @@ public class Client {
 		System.out.println("\t<plate number> <owner name> for register");
 		System.out.println("\t<plate number> for lookup");
 	}
+	
+	public static boolean procArgs(String[] args) {
+		if (args.length != 4 && args.length != 5)
+			return false;
+		
+		if (args[OPER].equals("register") && args.length == 5) {
+			hostname = args[HOST];
+			portNumber = Integer.parseInt(args[PORT]);
+			plateNumber = args[PLATE];
+			ownerName = args[OWNER];
 
-	public static void main(String[] args) {
+			return true;
+		} else if (args[OPER].equals("lookup") && args.length == 4) {
+			hostname = args[HOST];
+			portNumber = Integer.parseInt(args[PORT]);
+			plateNumber = args[PLATE];
+			
+			return true;
+		}
+		
+		return false;
+	}
+
+	public static void main(String[] args) throws IOException {
 		if (!procArgs(args))
 			printUsage();
-
-		// send request
-		// receive response
-		// process response
-		// tou a testar umas coisas
+		
+		byte[] rBuf = new byte[MAX_SIZE];
+		byte[] sBuf = new byte[MAX_SIZE];
+		
+		String request = null;
+		if (args[OPER].equals("register"))
+			request = "REGISTER:" + plateNumber + ":" + ownerName;
+		else if (args[OPER].equals("lookup"))
+			request = "LOOKUP:" + plateNumber;
+		
+		sBuf = request.getBytes();
+		
+		// Open socket, get address and create packet to store sent data
+		DatagramSocket socket = new DatagramSocket();
+		InetAddress address = InetAddress.getByName(hostname);
+		DatagramPacket sPacket = new DatagramPacket(sBuf, sBuf.length, address, portNumber);
+		socket.send(sPacket);
+		
+		// Create packet to store received data
+		DatagramPacket rPacket = new DatagramPacket(rBuf, rBuf.length);
+		socket.receive(rPacket);
+		
+		// Process response
+		String response = new String(rPacket.getData());
+		System.out.println("REPLY: " + response);
+		socket.close();
 	}
 	
 }
--- L01/Server.java
+++ L01/Server.java
@@ -1,27 +1,88 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.HashMap;
+
 public class Server {
 	
-	// Arg
-	private static String portNumber;
+	// Args
+	private static String reply;
+	private static int portNumber;
+	private static HashMap<String, String> dataBase;
 	
 	// Const
 	private static final byte PORT = 0;
+	private static final int MAX_SIZE = 256;
 	
 	public static void printUsage() {
 		System.out.println("java Server <port_number>");
 		System.out.println("Where:");
 		System.out.println("\t<port_number> is the port number on which the server waits for requests");
 	}
+	
+	public static void loadDataBase() {
+		try {
+			String line = null;
+			dataBase = new HashMap<>();
+			BufferedReader reader = new BufferedReader(new FileReader("res/db.txt"));
+			
+			while ((line = reader.readLine()) != null)
+				dataBase.put(line.split(":")[0], line.split(":")[1]);
+			
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void parseReceivedData(String data) {
+		String[] temp = data.split(":");
+		
+		if (temp[0].equals("REGISTER")) {
+			dataBase.put(temp[1], temp[2]);
+			reply = "" + dataBase.size();
+			return;
+		} else if (temp[0].equals("LOOKUP")) {
+			if (dataBase.get(temp[1]) != null) { // TODO
+				reply = temp[1] + dataBase.get(temp[1]);
+				return;
+			}
+		}
+		
+		reply = "-1";
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		if (args.length != 1)
 			printUsage();
-		else
-			portNumber = args[PORT];
+
+		loadDataBase();
+		byte[] rBuf = new byte[MAX_SIZE];
+		byte[] sBuf = new byte[MAX_SIZE];
+		
+		// Open socket
+		portNumber = Integer.parseInt(args[PORT]);
+		DatagramSocket socket = new DatagramSocket(portNumber);
 
 		while (true) {
-			// receive request
-			// process request
-			// respond
+			// Create packet to store receive data
+			DatagramPacket rPacket = new DatagramPacket(rBuf, rBuf.length);
+			socket.receive(rPacket);
+			
+			// Process received data
+			String rDataString = new String(rPacket.getData());
+			System.out.println("REQUEST: " + rDataString);
+			parseReceivedData(rDataString);
+			
+			// Send a response
+			sBuf = reply.getBytes();
+			int packetPort = rPacket.getPort();
+			InetAddress address = rPacket.getAddress();
+			DatagramPacket sPacket = new DatagramPacket(sBuf, sBuf.length, address, packetPort);
+			socket.send(sPacket);
 		}
 	}
 	
