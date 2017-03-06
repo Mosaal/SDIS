641db2bcb256018f581e07bf10222d2c52d9153c
Alexandre Saraiva Moreira <alexandremoreira_13@sapo.pt>
2017-03-06 17:28:14 +0000

    L02 done

--- L02/Client.java
+++ L02/Client.java
@@ -2,10 +2,16 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
 
 public class Client {
 
 	// Const
+	private static final byte ADDR = 0;
+	private static final byte PORT = 1;
+	private static final byte OPER = 2;
+	private static final byte PLATE = 3;
+	private static final byte OWNER = 4;
 	private static final int MAX_SIZE = 1024;
 
 	public static void printUsage() {
@@ -23,30 +29,58 @@ public class Client {
 		if (args.length != 4 && args.length != 5)
 			return false;
 
-		/*if (args[OPER].equals("register") && args.length == 5)
+		if (args[OPER].equals("register") && args.length == 5)
 			return true;
 		else if (args[OPER].equals("lookup") && args.length == 4)
-			return true;*/
+			return true;
 
 		return false;
 	}
 
 	public static void main(String[] args) throws IOException {
-		//if (!procArgs(args)) {
-			//printUsage();
-			//return;
-		//}
-		
-		String request = "Hello World";
+		if (!procArgs(args)) {
+			printUsage();
+			return;
+		}
 
-		// Create socket and get address
-		DatagramSocket socket = new DatagramSocket();
-		InetAddress address = InetAddress.getByName(args[0]);
+		// Creste request
+		String request = null;
+		if (args[OPER].equals("register"))
+			request = "REGISTER:" + args[PLATE] + ":" + args[OWNER];
+		else if (args[OPER].equals("lookup"))
+			request = "LOOKUP:" + args[PLATE];
 
-		// Send request
-		DatagramPacket output = new DatagramPacket(request.getBytes(), request.getBytes().length, address, Integer.parseInt(args[1]));
+		// Get address and open socket
+		InetAddress mAddress = InetAddress.getByName(args[ADDR]);
+		MulticastSocket mSocket = new MulticastSocket(Integer.parseInt(args[PORT]));
+		mSocket.joinGroup(mAddress);
+
+		// Receive port from Server
+		byte[] buf = new byte[MAX_SIZE];
+		DatagramPacket incoming = new DatagramPacket(buf, buf.length);
+		mSocket.receive(incoming);
+
+		// Parse informartion
+		String portData = new String(incoming.getData(), 0, incoming.getLength());
+		System.out.println("RECEIVED PORT: " + portData);
+
+		// Send out request
+		DatagramSocket sSocket = new DatagramSocket();
+		DatagramPacket output = new DatagramPacket(request.getBytes(), request.getBytes().length, incoming.getAddress(), Integer.parseInt(portData));
 		System.out.println("SENT REQUEST: " + request);
-		socket.send(output);
-		socket.close();
+		sSocket.send(output);
+
+		// Receive reply
+		buf = new byte[MAX_SIZE];
+		DatagramPacket reply = new DatagramPacket(buf, buf.length);
+		sSocket.receive(reply);
+
+		// Print out details of reply
+		String temp = new String(buf, 0, reply.getLength());
+		System.out.println("RECEIVED REPLY: " + temp);
+		
+		// Close sockets
+		mSocket.close();
+		sSocket.close();
 	}
 }
--- L02/Server.java
+++ L02/Server.java
@@ -5,16 +5,16 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
-import java.net.UnknownHostException;
 import java.util.HashMap;
 
 public class Server {
 
 	// Args
 	private static HashMap<String, String> dataBase;
-	
+
 	// Const
 	private static final byte SRVC_PORT = 0;
 	private static final byte MCAST_ADDR = 1;
@@ -28,7 +28,7 @@ public class Server {
 		System.out.println("\t<mcast_addr> is the IP address of the multicast group used by the server to advertise its service");
 		System.out.println("\t<mcast_port> is the multicast group port number used by the server to advertise its service");
 	}
-	
+
 	public static void loadDataBase() {
 		try {
 			String line = null;
@@ -43,17 +43,17 @@ public class Server {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void writeToFile(String plate, String owner) {
 		try (FileWriter fw = new FileWriter("db.txt", true);
-			BufferedWriter bw = new BufferedWriter(fw);
-			PrintWriter out = new PrintWriter(bw)) {
+				BufferedWriter bw = new BufferedWriter(fw);
+				PrintWriter out = new PrintWriter(bw)) {
 			out.print("\n" + plate + ":" + owner);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static String parseReceivedData(String data) {
 		String[] temp = data.split(":");
 
@@ -72,29 +72,74 @@ public class Server {
 		return "ERROR";
 	}
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		//if (args.length != 3) {
-			//printUsage();
-			//return;
-		//}
-		
-		// loadDataBase();
-		
-		InetAddress address = InetAddress.getByName("228.5.6.7");
-		MulticastSocket socket = new MulticastSocket(Integer.parseInt("6789"));
-		socket.joinGroup(address);
-		
-		while (true) {
-			byte[] buf = new byte[MAX_SIZE];
-			DatagramPacket packet = new DatagramPacket(buf, buf.length);
-			socket.receive(packet);
-			
-			String data = new String(packet.getData(), 0, packet.getLength());
-			System.out.println("RECEIVED: " + data);
-			if (data.equals("quit")) break;
+		if (args.length != 3) {
+			printUsage();
+			return;
 		}
-		
-		socket.leaveGroup(address);
-		socket.close();
+
+		loadDataBase();
+
+		// Open mCast socket
+		MulticastSocket mSocket = new MulticastSocket();
+		InetAddress mAddress = InetAddress.getByName(args[MCAST_ADDR]);
+		mSocket.setTimeToLive(1);
+
+		// Open server socket
+		DatagramSocket sSocket = new DatagramSocket(Integer.parseInt(args[SRVC_PORT]));
+
+		// Buffer to receive incoming data
+		byte[] buf = new byte[MAX_SIZE];
+		DatagramPacket incoming = new DatagramPacket(buf, buf.length);
+
+		// Thread for advertisement
+		(new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						// Create ad and send it
+						int mPort = Integer.parseInt(args[MCAST_PORT]);
+						DatagramPacket packet = new DatagramPacket(args[SRVC_PORT].getBytes(), args[SRVC_PORT].getBytes().length, mAddress, mPort);
+						mSocket.send(packet);
+						
+						// Print out info and wait for 1 sec
+						System.out.println("SENT PORT: " + args[SRVC_PORT]);
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		})).start();
+
+		// Thread for request
+		(new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						// Receive data
+						sSocket.receive(incoming);
+						byte[] data = incoming.getData();
+						String temp = new String(data, 0, incoming.getLength());
+
+						// Print out details of request
+						System.out.println("RECEIVED REQUEST: " + temp);
+						String reply = parseReceivedData(temp);
+
+						// Send reply
+						System.out.println("SENT REPLY: " + reply);
+						DatagramPacket output = new DatagramPacket(reply.getBytes(), reply.getBytes().length, incoming.getAddress(), incoming.getPort());
+						sSocket.send(output);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		})).start();
 	}
 }
