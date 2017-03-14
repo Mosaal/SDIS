8af44707177243407b546481e5d0c7b652660370
Alexandre Saraiva Moreira <alexandremoreira_13@sapo.pt>
2017-03-14 12:54:28 +0000

    Ok

---
+++ L04/Client.java
@@ -0,0 +1,47 @@
+public class Client {
+
+    // Const
+	private static final byte HOST = 0;
+	private static final byte REMT = 1;
+	private static final byte OPER = 2;
+	private static final byte PLATE = 3;
+	private static final byte OWNER = 4;
+
+    public static void printUsage() {
+		System.out.println("java Client <host_name> <remote_object_name> <oper> <opnd>");
+		System.out.println("Where:");
+		System.out.println("\t<host_name> is the name of the host where the server runs");
+		System.out.println("\t<remote_object_name> is the name the server bound the remote object to");
+		System.out.println("\t<oper> is 'register' or 'lookup', depending on the operation to invoke");
+		System.out.println("\nAnd where <opnd> is a list of arguments:");
+		System.out.println("\t<plate number> <owner name> for register");
+		System.out.println("\t<plate number> for lookup");
+	}
+
+    public static boolean procArgs(String[] args) {
+		if (args.length != 4 && args.length != 5)
+			return false;
+
+		if (args[OPER].equals("register") && args.length == 5)
+			return true;
+		else if (args[OPER].equals("lookup") && args.length == 4)
+			return true;
+
+		return false;
+	}
+
+    public static void main(String[] args) {
+        if (!procArgs(args)) {
+            System.out.println("ERROR: Wrong number of arguments.");
+            printUsage();
+			return;
+        }
+
+        // Create request
+		String request = null;
+		if (args[OPER].equals("register"))
+			request = "REGISTER:" + args[PLATE] + ":" + args[OWNER];
+		else if (args[OPER].equals("lookup"))
+			request = "LOOKUP:" + args[PLATE];
+    }
+}
---
+++ L04/Server.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+public class Server {
+
+    // Const
+	private static final byte REMT = 0;
+
+	// Args
+	private static HashMap<String, String> dataBase;
+
+    public static void printUsage() {
+        System.out.println("Usage: java Server <remote_object_name>");
+        System.out.println("Where:\n\t<remote_object_name> is the name the server bound the remote object to");
+    }
+
+    public static void loadDataBase() {
+		try {
+			String line = null;
+			dataBase = new HashMap<String, String>();
+			BufferedReader reader = new BufferedReader(new FileReader("db.txt"));
+
+			while ((line = reader.readLine()) != null)
+				dataBase.put(line.split(":")[0], line.split(":")[1]);
+
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void writeToFile(String plate, String owner) {
+		try (FileWriter fw = new FileWriter("db.txt", true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			PrintWriter out = new PrintWriter(bw)) {
+			out.print("\n" + plate + ":" + owner);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static String parseReceivedData(String data) {
+		String[] temp = data.split(":");
+
+		if (temp[0].equals("REGISTER")) {
+			if (!dataBase.containsKey(temp[1])) {
+				dataBase.put(temp[1], temp[2]);
+				writeToFile(temp[1], temp[2]);
+				return String.valueOf(dataBase.size());
+			}
+		} else if (temp[0].equals("LOOKUP")) {
+			if (dataBase.containsKey(temp[1])) {
+				return temp[1] + " -> " + dataBase.get(temp[1]);
+			}
+		}
+
+		return "ERROR";
+	}
+    
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("ERROR: Wrong number of arguments.");
+            printUsage();
+            return;
+        }
+
+        loadDataBase();
+    }
+}
