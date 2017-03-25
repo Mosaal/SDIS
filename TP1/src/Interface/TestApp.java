a414b735d484a18ce88a94fd6a84051d5e2a09ff
Alexandre Saraiva Moreira <alexandremoreira_13@sapo.pt>
2017-03-25 19:30:03 +0000

    Fixed

--- TP1/src/Interface/TestApp.java
+++ TP1/src/Interface/TestApp.java
@@ -1,23 +1,17 @@
 package Interface;
 
-import RMI.RMIInterface;
 import Utils.Utils;
-
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 
 public class TestApp {
 
 	/** Prints the correct way to initialize and execute an instance of this class */
 	private static void printUsage() {
-		System.out.println("Usage: java -cp ./bin Interface.TestApp <hostname>:<remote_object_name> <sub_protocol> <opnd_1> <opnd_2>");
+		System.out.println("Usage: java -cp ./bin Interface.TestApp <ip_address>:<port> <sub_protocol> <file_path> <rep_degree>");
 		System.out.println("Where:");
-		System.out.println("\t<hostname>:<remote_object_name> is the name of the host where the server runs followed by the name the server bound the remote object to");
+		System.out.println("\t<ip_address>:<port> is the address of the service Peer followed by its Port");
 		System.out.println("\t<sub_protocol> is the operation the peer of the backup service must execute (BACKUP, RESTORE, DELETE, RECLAIM, STATE)");
-		System.out.println("\t<opnd_1> is either the path name of the file to BACKUP/RESTORE/DELETE or the amount of space to reclaim (in KByte)");
-		System.out.println("\t<opnd_2> is an integer that specifies the desired replication degree and applies only to the BACKUP protocol");
+		System.out.println("\t<file_path> is either the path name of the file to BACKUP/RESTORE/DELETE or the amount of space to RECLAIM (in KByte)");
+		System.out.println("\t<rep_degree> is an integer that specifies the desired replication degree and applies only to the BACKUP protocol");
 	}
 
 	/**
@@ -25,25 +19,33 @@ public class TestApp {
 	 * @param args string array of arguments to be processed
 	 */
 	private static boolean procArgs(String[] args) {
+		// Check for minimum amount of arguments
 		if (args.length < 2) {
 			System.out.println("ERROR: Wrong number of arguments.");
 			return false;
 		}
 
+		// Check if the first argument has the correct format
 		if (!args[0].contains(":")) {
 			System.out.println("ERROR: The first argument has an incorrect format.");
 			return false;
 		}
 
+		// Check remaining arguments
 		switch (args.length) {
-		case 2: 
+		case 2:
 			if (!args[1].equals("STATE")) {
 				System.out.println("ERROR: '" + args[1] + "' is not a valid argument.");
 				return false;
 			}
 			break;
-		case 3: 
-			if ((args[1].equals("RESTORE")) || (args[1].equals("DELETE")) || (args[1].equals("RECLAIM"))) {
+		case 3:
+			if (args[1].equals("RECLAIM")) {
+				if (!Utils.isStringInteger(args[2])) {
+					System.out.println("ERROR: Argument '" + args[2] + "' is not an integer.");
+					return false;
+				}
+			} else if (args[1].equals("RESTORE") || args[1].equals("DELETE")) {
 				if (!Utils.fileExists(args[2])) {
 					System.out.println("ERROR: The file '" + args[2] + "' does not exist.");
 					return false;
@@ -53,7 +55,7 @@ public class TestApp {
 				return false;
 			}
 			break;
-		case 4: 
+		case 4:
 			if (args[1].equals("BACKUP")) {
 				if (!Utils.fileExists(args[2])) {
 					System.out.println("ERROR: The file '" + args[2] + "' does not exist.");
@@ -82,17 +84,7 @@ public class TestApp {
 			printUsage();
 			return;
 		}
-
-		try {
-			Registry registry = LocateRegistry.getRegistry("localhost");
-			RMIInterface stub = (RMIInterface) registry.lookup("kappa");
-
-			String str = stub.sayHello();
-			System.out.println(str);
-		} catch (RemoteException | NotBoundException e) {
-			e.printStackTrace();
-			printUsage();
-			System.exit(-1);
-		}
+		
+		System.out.println("Hello");
 	}
 }
--- TP1/src/Peer/Peer.java
+++ TP1/src/Peer/Peer.java
@@ -1,23 +1,16 @@
 package Peer;
 
-import RMI.RMIInterface;
-import RMI.RMIServer;
 import Utils.Utils;
-import java.rmi.AlreadyBoundException;
-import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
 
 public class Peer {
 
 	/** Prints the correct way to initialize and execute an instance of this class */
 	public static void printUsage() {
-		System.out.println("Usage: java -cp ./bin Peers.Peer <protocol_version> <server_id> <access_point> <mc_channel_ip>:<port> <mdb_channel_ip>:<port> <mdr_channel_ip>:<port>");
+		System.out.println("Usage: java -cp ./bin Peers.Peer <protocol_version> <server_id> <tcp_port> <mc_channel_ip>:<port> <mdb_channel_ip>:<port> <mdr_channel_ip>:<port>");
 		System.out.println("Where:");
 		System.out.println("\t<protocol_version> is the version of the protocol used");
-		System.out.println("\t<server_id> is the unique identifier of the server (must be an integer)");
-		System.out.println("\t<remote_object_name> is the name the server bound the remote object to");
+		System.out.println("\t<server_id> is the unique identifier of the server");
+		System.out.println("\t<tcp_port> is the TCP Port to which the TestApp shall connect to");
 		System.out.println("\t<mc_channel_ip>:<port> is the IP address of Multicast Control Channel followed by its Port");
 		System.out.println("\t<mdb_channel_ip>:<port> is the IP address of Multicast Data Backup Channel followed by its Port");
 		System.out.println("\t<mdr_channel_ip>:<port> is the IP address of Multicast Data Recovery Channel followed by its Port");
@@ -28,17 +21,25 @@ public class Peer {
 	 * @param args string array of arguments to be processed
 	 */
 	private static boolean procArgs(String[] args) {
+		// Check for minimum amount of arguments
 		if (args.length != 6) {
 			System.out.println("ERROR: Wrong number of arguments.");
 			return false;
 		}
 
+		// Check if server_id and tcp_port are integers
 		if (!Utils.isStringInteger(args[1])) {
 			System.out.println("ERROR: Argument '" + args[1] + "' is not an integer.");
 			return false;
 		}
+		
+		if (!Utils.isStringInteger(args[2])) {
+			System.out.println("ERROR: Argument '" + args[2] + "' is not an integer.");
+			return false;
+		}
 
-		if ((args[3].contains(":")) && (args[4].contains(":")) && (args[5].contains(":"))) {
+		// Check the format of the remaining arguments
+		if (args[3].contains(":") && args[4].contains(":") && args[5].contains(":")) {
 			if (!Utils.isStringInteger(args[3].split(":")[1])) {
 				System.out.println("ERROR: Port '" + args[3].split(":")[1] + "' is not an integer.");
 				return false; }
@@ -62,17 +63,7 @@ public class Peer {
 			printUsage();
 			return;
 		}
-
-		try {
-			RMIServer rmiServer = new RMIServer();
-			RMIInterface stub = (RMIInterface) UnicastRemoteObject.exportObject(rmiServer, 0);
-
-			Registry registry = LocateRegistry.getRegistry();
-			registry.bind("kappa", stub);
-		} catch (RemoteException | AlreadyBoundException e) {
-			e.printStackTrace();
-			printUsage();
-			System.exit(-1);
-		}
+		
+		System.out.println("Hi");
 	}
 }
