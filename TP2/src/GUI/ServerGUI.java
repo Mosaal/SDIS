8fb461d6c3d579714df7344cbc85c44f1e4b1cc6
Alexandre Saraiva Moreira <alexandremoreira_13@sapo.pt>
2017-04-10 17:16:14 +0100

    Comments

--- TP2/src/GUI/ServerGUI.java
+++ TP2/src/GUI/ServerGUI.java
@@ -5,7 +5,7 @@ import java.rmi.RemoteException;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import RMI.Server;
+// import RMI.Server;
 
 public class ServerGUI extends JFrame {
 
@@ -13,19 +13,19 @@ public class ServerGUI extends JFrame {
 	private static final long serialVersionUID = -4826945628298567633L;
 
 	// Instance variables
-	private Server server;
+	// private Server server;
 	private JPanel mainPanel;
-	private int currActiveRooms;
-	private int currActivePlayers;
+	// private int currActiveRooms;
+	// private int currActivePlayers;
 	
 	/** Creates a ServerGUI instance */
 	public ServerGUI() throws RemoteException {
 		super("Sueca - Server");
 		
-		currActiveRooms = 0;
-		currActivePlayers = 0;
+		// currActiveRooms = 0;
+		// currActivePlayers = 0;
 		
-		server = new Server();
+		// server = new Server();
 		mainPanel = new JPanel();
 		
 		setSize(600, 600);
